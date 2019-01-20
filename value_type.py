1cfec934866c7a18289fe6442d45e76c05f9118a
Jamey Hansen <jameyannehansen@gmail.com>
2019-01-19 19:40:20 -0600

    add value-type

---
+++ value_type.py
@@ -0,0 +1,27 @@
+#width//2
+
+#width/2.0
+
+#height/3
+
+#1 + 2 * 5
+width = 17
+height = 12.0
+
+answer1 = width//2
+print(answer1, type(answer1))
+
+
+
+answer2 = width/2.8
+type(answer2)
+print(answer2, type(answer2))
+
+answer3 = height/3
+type(height/3)
+print(answer3, type(answer3))
+
+answers=1 + 2 * 5
+
+print(answers, type(answers))
+
