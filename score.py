5bd57568377311c40a56946b972652aa751b9b5e
Jamey Hansen <jameyannehansen@gmail.com>
2019-01-27 18:54:52 -0600

    Add files via upload

---
+++ score.py
@@ -0,0 +1,29 @@
+prompt = ("Enter score: \n")
+
+
+try:
+	grade = input(prompt)
+	if .9 <=float(grade) <= 1:
+		print("A")
+	if .8 <= float(grade) < .9:
+		print("B")
+	if .7 <=float(grade) < .8:
+		print("C")
+	if .6 <=float(grade) < .7:
+		print("D")
+	if  float(grade) < .6:
+		print("F")
+	
+
+except:
+	print("Bad score!")
+
+
+def computegrade(score)
+	if .9 <=float(score) <= 1:
+	grade = "A"
+	return grade
+	if .8 <= float(grade) < .9:
+	grade = "B"
+	return grade
+	
